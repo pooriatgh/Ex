d6b1cfcc13bbc21203b2a75bafd3dd77cba6d121
Pooria <taghizadeh@kahkeshan.com>
2020-08-01 23:43:43 +0430

    init

---
+++ ExchangeC/Controllers/ProfileController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ExchangeC.Controllers
+{
+    public class ProfileController : Controller
+    {
+        // GET: Profile
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Register()
+        {
+            return View();
+        }
+    }
+}
---
+++ ExchangeC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ExchangeC.Startup))]
+namespace ExchangeC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
